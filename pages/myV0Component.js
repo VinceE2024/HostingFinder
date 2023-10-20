3d7655b15358124db4f6d8cf6e02988d7e7c4877
Vince Edwards <147632980+VinceE2024@users.noreply.github.com>
2023-10-20 10:37:49 -0700

    Initial commit

---
+++ pages/myV0Component.js
@@ -0,0 +1,13 @@
+//you can overwrite this entire file with your v0 component, this is just a placeholder.
+
+import React from 'react';
+
+function TestComponent() {
+    return (
+    <div class="hidden">
+        <h1>You can't see me but I'm going to be a v0 component.</h1>
+    </div>
+    )
+}
+
+export default TestComponent;
